97ae0299453f32e41e75eefe5a4d3bb2e4599563
Todo0901 <todoomn0901@gmail.com>
2025-12-24 14:42:29 +0800

    done

--- src/app/components/sections/Header.tsx
+++ src/app/components/sections/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <>
-    <div className='flex bg-black w-full h-20 items-center justify-between px-10  fixed overflow-hidden z-50'>
+    <div className='flex bg-black w-full h-20 items-center justify-between md:px-10 px-1 fixed overflow-hidden z-50'>
       <div className="relative w-10 h-10 sm:w-15 sm:h-15 rounded-full overflow-hidden border-2 border-cyan-400 shadow-lg">
           <Image
           src="/my-profile.png" 
@@ -14,12 +14,12 @@ const Header = () => {
           priority 
           />
         </div>
-        <div className=' flex text-white gap-5'>
-          <a href="#hero">Home</a>
+        <div className='flex text-white gap-5'>
+          <a href="#Hero">Home</a>
           <a href="#skill">Skills</a>
           <a href="#projects">Projects</a>
-          <a href='#game'>Mini Game</a>
-          <a href=""></a>
+          <a href="#game">Game</a>
+          <a href="#contact">Contacts</a>
         </div>
     </div>
     </>
